626e3da44f1be2b1b0723b167e72c9de2aba3c9e
weisiwu <2436887475@qq.com>
2024-06-01 19:26:39 +0800

    Initial Commit

---
+++ CapnoGraph/CapnoGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapnoGraphApp.swift
+//  CapnoGraph
+//
+//  Created by weisiwu on 2024/6/1.
+//
+
+import SwiftUI
+
+@main
+struct CapnoGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CapnoGraph/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CapnoGraph
+//
+//  Created by weisiwu on 2024/6/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
