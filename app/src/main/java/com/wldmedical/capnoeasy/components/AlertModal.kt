6175cca23cec2b99fb26868306bedfc3c1aaeb0c
weisiwu <siwu.wsw@autonavi.com>
2025-01-21 20:41:25 +0800

    feat: alertModal 完善

--- app/src/main/java/com/wldmedical/capnoeasy/components/AlertModal.kt
+++ app/src/main/java/com/wldmedical/capnoeasy/components/AlertModal.kt
@@ -1,21 +1,29 @@
 package com.wldmedical.capnoeasy.components
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.graphics.Paint.Align
 import android.icu.text.CaseMap.Title
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRowScopeInstance.align
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
@@ -27,8 +35,10 @@ import com.wldmedical.capnoeasy.ui.theme.CapnoEasyTheme
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.zIndex
 
 /**
@@ -38,7 +48,7 @@ import androidx.compose.ui.zIndex
 @Composable
 fun AlertModal(
     title: String? = null,
-    text: String,
+    text: String? = null,
     cancel_btn_text: String? = null,
     ok_btn_text: String? = null,
     onClick: ((Boolean) -> Boolean)? = null
@@ -64,50 +74,67 @@ fun AlertModal(
             },
     ) {
         Column {
-            Spacer(modifier = Modifier.weight(1f))
             Row(
                 modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)
             ) {
-                Spacer(modifier = Modifier.weight(1f))
-                AlertDialog(
-                    onDismissRequest = { showAlert.value = false },
-                    title = { if (title != null) Text(text = title) },
-//                    title = { if (title != null) Text(text = title, modifier = Modifier.align(Alignment.CenterHorizontally)) },
-                    text = {
-                        if (text != null) {
-                            Text(
-                                text = text,
-                                modifier = Modifier.weight(1f).border(width = 2.dp, color = Color.LightGray, shape = RoundedCornerShape(8.dp)),
-                                fontSize = 17.sp,
-                                color = Color(0xff1D2129)
-                            )
-                        }
-                    },
-                    shape = RoundedCornerShape(8.dp),
-                    confirmButton = {
-                        if (ok_btn_text != null) {
-                            Text(
-                                text = ok_btn_text,
-                                modifier = Modifier.weight(1f).border(width = 2.dp, color = Color.LightGray, shape = RoundedCornerShape(8.dp)),
-                                fontSize = 16.sp,
-                                color = Color(0xff165DFF)
-                            )
-                        }
-                    },
-                    dismissButton = {
-                        if (cancel_btn_text != null) {
-                            Text(
-                                text = cancel_btn_text,
-                                modifier = Modifier.weight(1f),
-                                fontSize = 16.sp,
-                                color = Color(0xff4E5969)
-                            )
+                Dialog(onDismissRequest = { showAlert.value = false }) {
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        shape = RoundedCornerShape(8.dp)
+                    ) {
+                        Column {
+                            if (title != null) {
+                                Column(modifier = Modifier.fillMaxWidth()) {
+                                    Text(
+                                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                                        text = title,
+                                    )
+                                }
+                            }
+                            if (text != null) {
+                                Column(modifier = Modifier.fillMaxWidth()) {
+                                    Text(
+                                        text = text,
+                                        modifier = Modifier.align(Alignment.CenterHorizontally).padding(top = 20.dp, bottom = 20.dp),
+                                        fontSize = 17.sp,
+                                        color = Color(0xff1D2129)
+                                    )
+                                }
+                            }
+                            if (ok_btn_text != null && cancel_btn_text != null) {
+                                Row(
+                                    modifier = Modifier.height(44.dp).align(Alignment.CenterVertically)
+                                ) {
+                                    Column(
+                                        modifier = Modifier.weight(1f).height(44.dp).border(width = 1.dp, color = Color(0x224E5969), shape = RoundedCornerShape(0.dp, 0.dp, 0.dp, 2.dp)),
+                                        verticalArrangement = Arrangement.Center
+                                    ) {
+                                        Text(
+                                            text = ok_btn_text,
+                                            modifier = Modifier.align(Alignment.CenterHorizontally),
+                                            fontSize = 16.sp,
+                                            color = Color(0xff165DFF)
+                                        )
+                                    }
+                                    Column(
+                                        modifier = Modifier.weight(1f).height(44.dp).border(width = 1.dp, color = Color(0x224E5969), shape = RoundedCornerShape(0.dp, 0.dp, 2.dp, 0.dp)),
+                                        verticalArrangement = Arrangement.Center
+                                    ) {
+                                        Text(
+                                            text = cancel_btn_text,
+                                            modifier = Modifier.align(Alignment.CenterHorizontally),
+                                            fontSize = 16.sp,
+                                            color = Color(0xff4E5969)
+                                        )
+                                    }
+                                }
+                            }
                         }
                     }
-                )
-                Spacer(modifier = Modifier.weight(1f))
+                }
             }
-            Spacer(modifier = Modifier.weight(1f))
         }
     }
 }
