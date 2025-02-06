5e015be2c07a078b5f1e7f07179ce157e6dbd2a3
weisiwu <siwu.wsw@autonavi.com>
2025-02-06 11:04:00 +0800

    feat: 修改为空图片展示问题

--- app/src/main/java/com/wldmedical/capnoeasy/components/DeviceList.kt
+++ app/src/main/java/com/wldmedical/capnoeasy/components/DeviceList.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,6 +23,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -29,6 +31,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
@@ -81,13 +84,14 @@ fun DeviceList(
                 Image(
                     painter = painterResource(R.drawable.empty_device_list),
                     contentDescription = emptyAlert,
-                    modifier = Modifier.padding(bottom = 16.dp)
+                    modifier = Modifier.size(150.dp).padding(bottom = 16.dp)
                 )
                 Text(
                     text = emptyAlert,
                     fontSize = 20.sp,
                     color = Color(0xFF687383)
                 )
+                Spacer(modifier = Modifier.fillMaxWidth().height(16.dp))
             }
         } else {
             LazyColumn(
@@ -137,9 +141,8 @@ fun DeviceList(
                     }
                 }
             }
+            Spacer(modifier = Modifier.weight(1f))
         }
-
-        Spacer(modifier = Modifier.weight(1f))
 
         Row(
             horizontalArrangement = Arrangement.Center,
@@ -181,6 +184,10 @@ fun DeviceList(
 fun DeviceListPreview() {
     val devices = listOf<BluetoothDevice>()
     CapnoEasyTheme {
-        DeviceList(devices = devices)
+        Row(
+            modifier = Modifier.background(Color.White).fillMaxSize()
+        ) {
+            DeviceList(devices = devices)
+        }
     }
 }
