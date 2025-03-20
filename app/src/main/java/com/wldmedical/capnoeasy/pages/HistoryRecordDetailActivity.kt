1c680a8600b9fbd94bd5ee746474bf6a8e2f1676
weisiwu <2436887475@qq.com>
2025-03-20 23:41:17 +0800

    feat: 注释掉历史记录详情页PDF预览相关代码

--- app/src/main/java/com/wldmedical/capnoeasy/pages/HistoryRecordDetailActivity.kt
+++ app/src/main/java/com/wldmedical/capnoeasy/pages/HistoryRecordDetailActivity.kt
@@ -4,19 +4,9 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.lifecycleScope
-import com.github.barteksc.pdfviewer.PDFView
 import com.wldmedical.capnoeasy.PageScene
 import com.wldmedical.capnoeasy.R
 import com.wldmedical.capnoeasy.components.ToastData
@@ -131,57 +121,19 @@ class HistoryRecordDetailActivity : BaseActivity() {
 
     @Composable
     override fun Content() {
-        val pdfFilePath: MutableState<String> = remember { mutableStateOf<String>("") }
         val recordId = intent.getStringExtra(recordIdParams)
-        val context = this;
-        var currentPage by remember { mutableStateOf(0) }
 
         LaunchedEffect(recordId) {
             lifecycleScope.launch {
                 withContext(Dispatchers.IO) {
                     val record = localStorageKit.database.recordDao().queryRecordById(UUID.fromString(recordId))
                     if (record != null) {
-//                        if (record.previewPdfFilePath != null) {
-//                            if (record.previewPdfFilePath!!.isNotEmpty()) {
-//                                pdfFilePath.value = record.previewPdfFilePath!!
-//                                context.sourceFilePath = record.pdfFilePath!!
-//                                context.saveFileName = "${record.patientIndex}_${record.dateIndex}"
-//                                context.currentRecord = record
-//                            }
-//                        }
+
                     }
                 }
             }
         }
 
-
-
-//        if (pdfFilePath.value != "") {
-        // 放弃通过PDF预览历史波形数据，但是保留代码
-        if (false) {
-            AndroidView(
-                factory = { context ->
-                    val pdfFile = File(pdfFilePath.value)
-                    PDFView(context, null).apply {
-                        fromFile(pdfFile)
-                            .defaultPage(currentPage) // 设置默认显示的页面
-                            .onPageChange { page, pageCount ->
-                                currentPage = page
-                            }
-                            .enableAnnotationRendering(true) // 启用注释渲染（如果有）
-                            .swipeHorizontal(true) // 启用水平滑动
-                            .pageSnap(true) // 每次只显示一页
-                            .autoSpacing(true) // 自动调整页面间距
-                            .pageFling(true) // 启用页面快速滑动
-                            .load()
-
-                    }
-                },
-                modifier = Modifier
-                    .fillMaxSize()
-            )
-//        } else {
-//            Text(getStringAcitivity(R.string.recorddetail_loading))
-        }
+        // 渲染波形图
     }
 }
