6d915babd5b6c13bf81f78f7eb8aa112c5fba9b6
weisiwu <2436887475@qq.com>
2025-03-27 00:51:10 +0800

    feat: 修改最低版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.wldmedical.capnoeasy"
-        minSdk = 26
+        minSdk = 33
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
