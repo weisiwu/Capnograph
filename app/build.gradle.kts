088de6ed1e37294b2399b95854366743f11b9d4d
weisiwu <2436887475@qq.com>
2026-05-12 01:45:53 +0800

    chore: 升级版本到1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.wldmedical.capnoeasy"
         minSdk = 30
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
